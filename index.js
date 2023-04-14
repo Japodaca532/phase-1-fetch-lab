ed0908a4bf6946083a9be54653371a25a61bfd17
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-04-14 16:59:39 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json)
-  .then((json) => renderBooks(json));
+  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
